5f9e00d36b2f17747335b3a653877658fd36c23c
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-21 14:56:03 +0900

    increment components using multiple statements

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
+  const initialStates = {
+    name: '',
+    price: 1000
+  }
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const devide3 = () => setCount(previousCount => {
-    if (previousCount % 3 === 0) {
-      return previousCount / 3
-    } else {
-      return previousCount;
-    }
-  })
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-
-  const calculate2 = () => setCount(count * 2)
-
-
-
-  const reset = () => setCount(0)
-  // useStateは配列を返す
-  // 引数には初期値を入れる
   return (
     <>
-      <div className="App">
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>reset</button>
-        <button onClick={calculate2}>*2</button>
-        <button onClick={devide3}>3の時だけ3でわる</button>
-      </div>
+      <p>
+        現在の{name}は{price}円です。
+        <button onClick={() => setPrice(price + 1)}>+1</button>
+        <button onClick={() => setPrice(price - 1)}>-1</button>
+        <button onClick={reset}>Reset</button>
+        <input value={name} onChange={e => setName(e.target.value)} />
+      </p>
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
