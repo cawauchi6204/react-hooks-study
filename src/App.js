1c6c66d7e64ae1238f5270ab4af63f032eb18dcc
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-21 14:39:59 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const devide3 = () => setCount(previousCount => {
+    if (previousCount % 3 === 0) {
+      return previousCount / 3
+    } else {
+      return previousCount;
+    }
+  })
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+  const calculate2 = () => setCount(count * 2)
+
+
+
+  const reset = () => setCount(0)
+  // useStateは配列を返す
+  // 引数には初期値を入れる
   return (
-    <div className="App">
-      This is a Template for React App
-    </div>
+    <>
+      <div className="App">
+        count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+        <button onClick={calculate2}>*2</button>
+        <button onClick={devide3}>3の時だけ3でわる</button>
+      </div>
+    </>
   );
 }
 
