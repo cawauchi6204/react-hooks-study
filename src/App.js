97e3aac00647216556a6d6be42fde7b3ab4e7d63
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-21 15:25:02 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = props => {
-  const initialStates = {
-    name: '',
-    price: 1000
-  }
 
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+  const [state, setState] = useState(props)
+  const { name, price } = state
+  // この分割代入の知識が浅い
 
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate')
+  })
+  useEffect(() => {
+    console.log('This callback is for name only')
+  }, [name])
+  // 第一引数に関数を受け取れる
+  // 第二引数にからの配列を渡すとcomponentDidMountは実行されない
+  // 第二引数の配列にプロバティを渡すとそのコンポーネントに変化があったときのみレンダリングされる
+  // 状態が変わるごとにレンダリングが行われる
+  // useEffectはレンダリングのあとで実行される
+
 
   return (
     <>
       <p>
         現在の{name}は{price}円です。
-        <button onClick={() => setPrice(price + 1)}>+1</button>
-        <button onClick={() => setPrice(price - 1)}>-1</button>
-        <button onClick={reset}>Reset</button>
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+        <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
+        <button onClick={() => setState(props)}>Reset</button>
+        <input value={name} onChange={e => setState({ ...state, name: e.target.value })} />
       </p>
     </>
   );
