848ae8a6a5f69a94b1a34d1f78a69182647d45bb
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-27 11:50:52 +0900

    solve prop drilling issue

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,18 @@ import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from "./EventForm"
-
 import Events from './Events'
 import AppContext from "../contexts/AppContext"
 import reducer from '../reducers/index'
-
-console.log({ AppContext });
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <AppContext.Provider value={'Hello , I am a Provider.'}>
+    <AppContext.Provider value={{ state, dispatch }}>
       <div className="App container-fluid">
-        <EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   );
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 
 import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions"
 
-const EventForm = ({ state, dispatch }) => {
-    // const [state, dispatch] = useReducer(reducer, [])
-    //  第一引数にはreducerを渡す
-    //  第二引数にはdefaultの状態の指定
-    //  第三引数は初期化時のみ発火したいコールバックがあれば指定する
+import AppContext from '../contexts/AppContext'
+
+const EventForm = () => {
+    const { state, dispatch } = useContext(AppContext)
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     // reducerで扱うものはevent
